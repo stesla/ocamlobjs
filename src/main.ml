a675eaa202d57c60c4fbf24d2f6d40c71a10a4c3
Samuel Tesla <samuel@Sachi.local>
2008-03-03 16:30:31 -0600

    Refactor out a parse function and a Set type.

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,3 @@
-let last_char str = String.get str (String.length str - 1)
-
 let rec read_line_with_continue str =
   let line = read_line () in
   let regexp = Str.regexp "^\\(.*\\)\\\\$" in
@@ -16,13 +14,26 @@ let split_dep_line str =
     [file; deps] -> (file, Str.split (Str.regexp "[ \t]+") deps)
   | _ -> raise Invalid
 
-let _ =
+module S = Set.Make(
+  struct
+    type t = string * string list
+    let compare x y = match (x,y) with ((x',_),(y',_)) -> compare x' y'
+  end)
+
+let rec parse_dependencies deps =
   try
-    while true do
-      let line = read_line_with_continue "" in
-      let (file, deps) = split_dep_line line in
-      print_endline file;
-      print_endline (List.fold_left (fun s x -> s ^ "\t" ^ x ^ "\n") "" deps)
-    done
+    let line = read_line_with_continue "" in
+    let dep = split_dep_line line in
+    parse_dependencies (S.add dep deps)
   with End_of_file ->
-    exit 0
+    deps
+
+let _ =
+  let deps = parse_dependencies S.empty in
+  let f x =
+    match x with
+      (dep, deps) ->
+        print_endline dep;
+        print_endline (List.fold_left (fun s x -> s ^ "\t" ^ x ^ "\n") "" deps)
+  in
+  S.iter f deps
